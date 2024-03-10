63d5d70e0e9dc41ec0d7b97babff365da2fdda30
Przem0s84 <przemyslaw.wacek202@gmail.com>
2024-03-10 20:26:39 +0100

    fix PE consideration

--- PK_ScheduleScraper/Controllers/ScheduleController.cs
+++ PK_ScheduleScraper/Controllers/ScheduleController.cs
@@ -30,7 +30,7 @@ namespace PK_ScheduleScraper.Controllers
             if(!ModelState.IsValid) { return BadRequest(ModelState); }
             var scrappedList = await _scraperService.ScrapEventList(eventProp.TeamName);
 
-            var myEvents = scrappedList.Where(e => e.EventType == eventProp.LabGropName|| e.EventType == eventProp.ProjGroupName || e.EventType == eventProp.CompLabGroupName||e.EventType=="W"||e.EventType=="Ć"||e.EventType=="S" ||e.EventType.Contains("(K") ||e.EventType.Contains("M")||e.EventType.Contains("undefined")).Where(e=>e.WeekType==eventProp.WeekType).OrderBy(e=>e.DayNr).ThenBy(e=>e.EventLP).ToList();
+            var myEvents = scrappedList.Where(e => e.EventType == eventProp.LabGropName|| e.EventType == eventProp.ProjGroupName || e.EventType == eventProp.CompLabGroupName||e.EventType=="W"||e.EventType=="Ć"||e.EventType=="S" ||e.EventType.Contains("(K") ||e.EventType.Contains("M")||e.EventType.Contains("undefined")).Where(e=>e.WeekType==eventProp.WeekType||e.WeekType=="undefined or both").OrderBy(e=>e.DayNr).ThenBy(e=>e.EventLP).ToList();
 
 
             return Ok(myEvents);
